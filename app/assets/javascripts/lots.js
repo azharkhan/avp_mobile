0a75d79acf549d0f080dc2236b5af602932890d2
Azhar Khan <azmanian.devil@gmail.com>
2013-03-18 01:05:08 -0400

    last minute BS

--- app/assets/javascripts/lots.js
+++ app/assets/javascripts/lots.js
@@ -1,21 +1,39 @@
 var ip = 'http://192.168.137.37/'
 
-$(document).on('click', '#park_button', function () {
+$(document).on('click', '#car_park_button', function () {
 	var bay_number = $('#bay_number').val();
 	$.ajax({
 		type: 'GET',
 		data: bay_number,
 		dataType: 'jsonp',
-		url: ip+'p/'
+		url: ip
 	});
 });
 
-$(document).on('click', '#retrieve_button', function () {
+$(document).on('click', '#car_retrieve_button', function () {
 	var ticket_number = $('#ticket_number').val();
 	$.ajax({
 		type: 'GET',
 		data: ticket_number,
 		dataType: 'jsonp',
-		url: ip+'r/'
+		url: ip
+	});
+});
+
+$(document).on('click', '#park_button', function () {
+	$.ajax({
+		type: 'GET',
+		data: 'p/'
+		dataType: 'jsonp',
+		url: ip
+	});
+});
+
+$(document).on('click', '#retrieve_button', function () {
+	$.ajax({
+		type: 'GET',
+		data: 'r/',
+		dataType: 'jsonp',
+		url: ip
 	});
 });
